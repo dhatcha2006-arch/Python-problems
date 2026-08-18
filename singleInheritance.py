97585335262f1337dfd4cdc3b096f1acf66a724e
Dhatchanamoorthi A <dhatcha2006@gmail.com>
2026-08-18 14:20:35 +0530

    Single Inheritance

---
+++ singleInheritance.py
@@ -0,0 +1,12 @@
+# single parent single child
+
+class dad():
+    def phone(self):
+        print("Dad's phone")
+
+class son(dad):
+    def laptop(self):
+        print("Son's Laptop")
+
+s1=son()
+s1.phone()
